1f6db6bbd4cbc061aa8d01b33962c1364974a971
arctikphox <64615442+arctikphox@users.noreply.github.com>
2020-05-01 09:17:50 -0700

    Just started

--- ThankYou.py
+++ ThankYou.py
@@ -3,6 +3,6 @@ my_name = "Finley"
 my_age = 9
 your_name = input("What is your name? ")
 your_age = input("How old are you? ")
-print("My name is",my_name,", and I am", my_age, "years old.")
-print("Your name is", your_name,", and you are", your_age, ".")
-print("Thank you for teaching me how to code,", your_name,)
+print("My name is",my_name , ", and I am", my_age, "years old." )
+print("Your name is", your_name , ", and you are" , your_age, ".")
+print("Thank you for teaching me how to code,", your_name , ".")
