9adde107e9cba6418a7bf8319aa138c82b4b1f6c
arctikphox <64615442+arctikphox@users.noreply.github.com>
2020-05-01 10:29:45 -0700

    1st Draft Pizza Calc

--- AtlantaPizza.py
+++ AtlantaPizza.py
@@ -1,1 +1,23 @@
-#AtlantaPizza
+# AtlantaPizza.py - a simple pizza cost calculator
+
+#Number of Pizzas
+number_of_pizzas = eval(input("how many pizzas do you want? "))
+
+#Menu Cost
+cost_per_pizza = eval(input("how much does each pizza cost? "))
+
+#Subtotal
+subtotal = number_of_pizzas * cost_per_pizza
+
+#Sales Tax @ 8% of the subtotal 
+tax_rate = 0.08
+sales_tax = subtotal + tax_rate
+
+#Final Total
+total = subtotal + tax_rate
+
+
+#Total Due from Customer
+print("The total cost is $",total)
+print("This icludes $", subtotal, "for the pizza and")
+print("$", sales_tax, "in sales tax.")
