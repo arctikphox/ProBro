dd8bdbebb9a46cb00369a2d070528337b8c01815
arctikphox <64615442+arctikphox@users.noreply.github.com>
2020-04-30 18:14:59 -0700

    Issue with Text Spacing

---
+++ ThankYou.py
@@ -0,0 +1,8 @@
+#ThankYou.py
+my_name = "Finley"
+my_age = 9
+your_name = input("What is your name? ")
+your_age = input("How old are you? ")
+print("My name is",my_name,", and I am", my_age, "years old.")
+print("Your name is", your_name,", and you are", your_age, ".")
+print("Thank you for teaching me how to code,", your_name,)
